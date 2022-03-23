b4bc5d35dc674eea07237776e99fea1820b764ad
Fahim Ur Rahman <frahman66660@gmail.com>
2022-03-23 18:32:24 +0600

    FAQ toggling CSS, JS update.

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 //changing navbar styles on scroll
 
-window.addEventListener('scroll', () =>{
+window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
-    ('window-scroll', window.scrollY> 0)
+        ('window-scroll', window.scrollY > 0)
 })
+
+//show/hide FAQ answers
+
+const faqs = document.querySelectorAll('.faq')
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus-circle'){
+            icon.className = "uil uil-minus-circle";
+        }
+        else{
+            icon.className = "uil uil-plus-circle"
+        }
+    })
+});
